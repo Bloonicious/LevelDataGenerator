5c6b12b122821d2b1c501b4ca1442396d41c6e9d
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-04-20 20:58:41 +0100

    Update statistics.js

--- statistics.js
+++ statistics.js
@@ -9,20 +9,29 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // Listen for changes in mineshaft level data
     document.addEventListener('mineshaftDataChanged', function(event) {
-        totalLevels = levelData_mineshaft.length;
+        totalLevels += levelData_mineshaft.length;
         updateTotalLevels();
     });
 
     // Listen for changes in elevator level data
     document.addEventListener('elevatorDataChanged', function(event) {
-        totalLevels = levelData_elevator.length;
+        totalLevels += levelData_elevator.length;
         updateTotalLevels();
     });
 
     // Listen for changes in warehouse level data
     document.addEventListener('warehouseDataChanged', function(event) {
-        totalLevels = levelData_warehouse.length;
+        totalLevels += levelData_warehouse.length;
         updateTotalLevels();
+    });
+
+    // Listen for changes in levelsToGenerateInput box for warehouse
+    document.getElementById('levelsToGenerateInput').addEventListener('change', function(event) {
+        let levelsToGenerate = parseInt(event.target.value);
+        if (!isNaN(levelsToGenerate)) {
+            totalLevels += levelsToGenerate;
+            updateTotalLevels();
+        }
     });
 
     // Update total levels count on page load
