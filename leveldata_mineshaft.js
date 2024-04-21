38ad58923ccc5e95444543eb4759a5d9ce8c0e52
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-04-21 16:02:29 +0100

    Update leveldata_mineshaft.js

--- leveldata_mineshaft.js
+++ leveldata_mineshaft.js
@@ -94,12 +94,20 @@ let workerCountIncrementLevel = {
     1501: 8
 };
 
+document.addEventListener("DOMContentLoaded", function() {
+    // Add event listener to tier input for generating levels
+    var tierInput = document.getElementById("tierInput");
+    tierInput.addEventListener("input", generateLevels_mineshaft);
+
+    // Generate levels when the page loads
+    generateLevels_mineshaft();
+});
+
 function generateLevels_mineshaft() {
     var selectedGenerator = generatorBehaviorSelect.value;
 
     // Common input IDs
     var levelInput = document.getElementById("mineshaftLevelInput");
-    var tierInput = document.getElementById("tierInput");
 
     // Variables for specific input IDs
     var costInput, superCashInput, skillPointIDInput, gainInput, capacityInput;
