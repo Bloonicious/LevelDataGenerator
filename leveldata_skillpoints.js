27125c14b192517cbd305f108a08d8c7df64ad84
Bloonicious <155857894+Bloonicious@users.noreply.github.com>
2024-07-26 22:07:52 +0100

    Update leveldata_skillpoints.js

--- leveldata_skillpoints.js
+++ leveldata_skillpoints.js
@@ -9,11 +9,13 @@ let skillpointCostMultiplier816 = 1.1;
 let skillpointCostMultiplier2501 = 1.2;
 
 function generateLevels_skillpoints() {
+    // Read and parse input values
     let currentLevel = parseInt(document.getElementById('skillPointIDInput').value);
     let currentSkillpointCost = parseFloat(document.getElementById('skillpointCostInput').value);
     let currentSuperCashCost = parseFloat(document.getElementById('skillpointSuperCashCostInput').value);
     let levelsToGenerate = parseInt(document.getElementById('levelsToGenerateInput').value);
 
+    // Initialize last level object
     let lastLevel = {
         "0 Param data": {
             "0 SInt64 SkillPointNo": currentLevel - 1,
@@ -22,38 +24,44 @@ function generateLevels_skillpoints() {
         }
     };
 
+    // Generate levels
     for (let i = 0; i < levelsToGenerate; i++) {
         let newLevel = {};
         newLevel["0 Param data"] = {};
-        newLevel["0 Param data"]["0 SInt64 SkillPointNo"] = lastLevel["0 Param data"]["0 SInt64 SkillPointNo"] + 1;
 
-        // Determine the correct multiplier based on the skillpoint id
-        let skillPointNo = newLevel["0 Param data"]["0 SInt64 SkillPointNo"];
+        // Calculate new skillpoint number as a number
+        let newSkillPointNo = lastLevel["0 Param data"]["0 SInt64 SkillPointNo"] + 1;
+        newLevel["0 Param data"]["0 SInt64 SkillPointNo"] = newSkillPointNo;
+
+        // Determine the correct cost multiplier based on the skillpoint number
         let currentCostMultiplier;
-        if (skillPointNo < 31) {
+        if (newSkillPointNo < 31) {
             currentCostMultiplier = skillpointCostMultiplier;
-        } else if (skillPointNo < 101) {
+        } else if (newSkillPointNo < 101) {
             currentCostMultiplier = skillpointCostMultiplier31;
-        } else if (skillPointNo < 151) {
+        } else if (newSkillPointNo < 151) {
             currentCostMultiplier = skillpointCostMultiplier101;
-        } else if (skillPointNo < 501) {
+        } else if (newSkillPointNo < 501) {
             currentCostMultiplier = skillpointCostMultiplier151;
-        } else if (skillPointNo < 816) {
+        } else if (newSkillPointNo < 816) {
             currentCostMultiplier = skillpointCostMultiplier501;
-        } else if (skillPointNo < 2501) {
+        } else if (newSkillPointNo < 2501) {
             currentCostMultiplier = skillpointCostMultiplier816;
         } else {
             currentCostMultiplier = skillpointCostMultiplier2501;
         }
 
+        // Calculate new costs
         newLevel["0 Param data"]["0 double Cost"] = lastLevel["0 Param data"]["0 double Cost"] * currentCostMultiplier;
         newLevel["0 Param data"]["0 double SuperCashCost"] = lastLevel["0 Param data"]["0 double SuperCashCost"];
 
         // Convert the skillpoint ID to a string for storage in the JSON output
-        newLevel["0 Param data"]["0 SInt64 SkillPointNo"] = skillPointNo.toString();
+        newLevel["0 Param data"]["0 SInt64 SkillPointNo"] = newSkillPointNo.toString();
 
-        // Push the new level data
+        // Add new level data to the array
         levelData_skillpoints.push(newLevel);
+
+        // Update lastLevel for the next iteration
         lastLevel = newLevel;
     }
 
@@ -68,7 +76,6 @@ function displayLevels_skillpoints() {
 
 function copyJsonSkillpoint() {
     let filename = `skillpoint_data.json`;
-    let outputDiv = document.getElementById('output');
     let json = JSON.stringify(levelData_skillpoints, null, 4);
     let blob = new Blob([json], { type: 'application/json' });
     let url = URL.createObjectURL(blob);
